0dbc510e555f92c8ad8904c885d1efaefc0baa3e
nbaradar <naderb1992@gmail.com>
2021-03-10 17:51:07 -0500

    comments for learning matplotlib

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,16 @@
+#Aliasing in python, short hand for pyplot
 import matplotlib.pyplot as plt
 
 #create a list called square
 squares = [1, 4, 9, 16, 25]
 
+#Common matplotlib convention
+#This function generates 1 or more plots in the same figure. 
+#It returns a tuple containing a figure and axes object(s)
+#figure represents the entire figure or collection of plots
+#ax represents a single plot in the figure and is the variable you use most of the time
 fig, ax = plt.subplots()
 ax.plot(squares)
 
+#.show() opens matplotlibs viewer and displays the plot
 plt.show()
