f8686e06b97ae82cf062d7e601b2b642d3c57ac5
nbaradar <naderb1992@gmail.com>
2021-03-11 17:07:20 -0500

    Plot a series of points by passing in lists to ax.scatter()

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
 #plots a single point at the passed in coordinates
 #The s argument sets the size of the dots used to draw the graph
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 #Set Chart title and label axes
 ax.set_title("Square Numbers", fontsize = 24)
