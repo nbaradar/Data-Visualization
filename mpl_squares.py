f2d27403d8eef7273a37f86e763bf5ea727c47cf
nbaradar <naderb1992@gmail.com>
2021-03-10 22:06:00 -0500

    Changed visualization

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,8 @@
 #Aliasing in python, short hand for pyplot
 import matplotlib.pyplot as plt
 
-#create a list called square
+#x/y values of graph
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 #Common matplotlib convention
@@ -10,7 +11,16 @@ squares = [1, 4, 9, 16, 25]
 #figure represents the entire figure or collection of plots
 #ax represents a single plot in the figure and is the variable you use most of the time
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+#You can adjust visualization of all features in matplotlib
+#Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
 
 #.show() opens matplotlibs viewer and displays the plot
 plt.show()
