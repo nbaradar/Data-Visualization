ee3108a30a5ffe7e90abbcdf0adec2b63250fd24
nbaradar <naderb1992@gmail.com>
2021-03-11 22:49:34 -0500

    added color/colormap to axes

--- scatter_squares.py
+++ scatter_squares.py
@@ -16,12 +16,20 @@ x_values = range(1, 1001)
 #   newlist = [expression for item in iterable if condition == True]
 y_values = [x**2 for x in x_values]
 
+test_x = range(1, 1001)
+test_y = [(x**2)+(x*20) for x in test_x]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
 #plots a single point at the passed in coordinates
 #The s argument sets the size of the dots used to draw the graph
-ax.scatter(x_values, y_values, s=10)
+#The c argument sets the color of the dots used to draw the graph. You can also use rgb tuple. lower = darker; ex. (0, 0.8, 0)
+ax.scatter(x_values, y_values, c='red', s=10)
+
+#Colormaps are a series of colors in a gradient. Used to emphasize pattern in data
+#You must tell pyplot to assign a color to each point in the data set
+ax.scatter(test_x, test_y, c=test_y, cmap=plt.cm.Blues, s=10)
 
 #Set Chart title and label axes
 ax.set_title("Square Numbers", fontsize = 24)
