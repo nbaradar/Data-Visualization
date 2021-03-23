9301c0c7bc65d495578b6f5a39a24a2102a57389
nbaradar <naderb1992@gmail.com>
2021-03-23 16:07:44 -0400

    Rolling 2 D6 and plotting results

--- die_visual.py
+++ die_visual.py
@@ -5,41 +5,54 @@ from plotly.graph_objs import Bar, Layout
 
 #Create a D6
 d6 = Die(6)
+d6_2 = Die(6)
 
 #Make some rolls, and store them in a list
 results = []
+results2Die = []
+
 for roll_num in range(1000):
     result = d6.roll()
-    results.append(result)
+    results.append(result) 
+
+for roll_num in range(1000):
+    result = d6.roll() + d6_2.roll()
+    results2Die.append(result)
 
 #Analyze the results. Return the frequency of the rolls using .count() and store in a list
 #.count() lets you pass in any type to search for
+largestPossibleRoll = d6.num_sides + d6_2.num_sides;
 frequencies = []
-for side in range(1, d6.num_sides+1):
+""" for side in range(1, d6.num_sides+1):
     frequency = results.count(side)
+    frequencies.append(frequency) """
+
+for side in range(2, largestPossibleRoll+1):
+    frequency = results2Die.count(side)
     frequencies.append(frequency)
 
 #Visualize the results by creating a histogram using plotly
 #The xvalues along the bottom of the graph will be the number of sides
-x_values = list(range(1, d6.num_sides+1))
+x_values = list(range(2, largestPossibleRoll+1))
 #Bar() represents a data set that will be formatted as a bar chart
 #this class needs list of xvals and yvals
 #It is wrapped around brackets because a data set can have multiple elements
 data = [Bar(x=x_values, y=frequencies)]
 
 #These dictionary entries store the axis configurations
-x_axis_config = {'title':'Result'}
+#the dtick setting controls the spacing between tick marks on the x-axis
+x_axis_config = {'title':'Result', 'dtick':1}
 y_axis_config = {'title':'Frequency of Result'}
 
 #derive the title from the num of sides and amount of rolls
-my_title = 'Results of rolling one D' + str(d6.num_sides)+ ' ' + str(sum(frequencies))+ ' times'
+my_title = 'Results of rolling two D' + str(d6.num_sides)+ ' ' + str(sum(frequencies))+ ' times'
 
 #Layout() returns an object that specifies the layout and configs of the graph
 #We set the title, and xaxis/yaxis configs
 my_layout = Layout(title = my_title, xaxis = x_axis_config, yaxis = y_axis_config)
 
 #plot() needs a dictionary containing the data to plot and layout objects. Also accepts filename to store graph
-offline.plot({'data':data, 'layout':my_layout,}, filename='d6.html')
+offline.plot({'data':data, 'layout':my_layout,}, filename='d6x2.html')
 
 #print(results)   
 print(frequencies)
